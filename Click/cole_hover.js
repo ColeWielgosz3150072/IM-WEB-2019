f88c42d58fa3b3348b945c87eb73c06febcfe6c1
Cole Wielgosz <3150072@student.ocadu.ca>
2019-11-13 00:57:50 -0500

    so, so much

--- Click/cole_hover.js
+++ Click/cole_hover.js
@@ -2,82 +2,34 @@
 
 
 function move() {
-    $('.moveMe').css({
-    	"top":Math.random()*200 + 'px',
-    	"left":Math.random()*200 + 'px'
+    $(this).parents().css({
+    	"top":20 + (Math.random()*20) + 'vh',
+    	"left":50 + (Math.random()*20) + 'vw'
+    })
+}
+function move2(){
+      $(this).parents().css({
+      "top":(Math.random()*85) + 'vh',
+      "left":(Math.random()*85) + 'vw',
+      "transition": '0.5s'
     })
 }
 
+function yahoo() { 
+         $('<div class ="yeehaw2 bgSettings"><button class = "moveMe2 test2 bgSettings"></button></div>').appendTo($('.bigBoy'));    
+        } 
+
 $(document).ready(function() {
     $('.moveMe').hover(move);
+    $('.moveMe').click(yahoo);
+    $(document).on('mouseenter', '.moveMe2', move2);
 });
 
+$(document).on('click', '.moveMe2', yahoo);
 
+//in trouble shooting, it was suggested to adress
+//appended elements with the on. method rather than
+//the regular jquery method. So I initially got window1
+//working with $().hover, but had to use 'mouseenter'
+//on the subsequent appended divs
 
-
-
-
-
-
-
-
-
-
-  // $(document).ready(function(){
-  //   $('.box').hover(function() {
-  //   $(this).box;
-  //   $(this).css('position','absolute').css('top', Math.random()*200 + 'px').css('left', Math.random()*200 + 'px');
-  //   });
-  //   });
-
-
-
-//    $(document).ready(function(){
-//     $('.box').hover(function() {
-    // $(this).css({
-    // 	"top":Math.random()*200 + 'px',
-    // 	"left":Math.random()*200 + 'px'
-    // })
-
-//     })
-// })
-
-
-
-// $(document).mousemove(function(e){
-// 	var mX = e.clientX -60
-// 	var mY = e.clientY
-
-// 	$(".countup").css({
-// 		"top":mY,
-// 		"left":mX
-// 	})
-
-// })
-
-
-// var box = $(".box");
-
-// function moveItOrLoseIt(){
-// 	box.stop().animate({
-// 		"top":Math.random()*200 + 'px',
-//     	"left":Math.random()*200 + 'px'
-// 	});
-// }
-
-
-
-//   $(document).ready(function(){
-    
-//     $('.box').hover(function() {
-
-//     	moveItOrLoseIt()
- 
-//   })
-// })
-
-
-    // $(this).css({
-    // 	"top":Math.random()*200 + 'px',
-    // 	"left":Math.random()*200 + 'px'
-    // })
